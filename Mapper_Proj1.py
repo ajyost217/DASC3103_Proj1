f473131242f67dc1ade2dc95e230a6b29c4b1659
ajyost217 <101150640+ajyost217@users.noreply.github.com>
2023-10-01 14:09:41 -0500

    Add files via upload

--- Mapper_Proj1.py
+++ Mapper_Proj1.py
@@ -1,6 +1,10 @@
-import sys
-
 def map_function(line):
+    expected_headers = "Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age,Outcome"
+    
+    if line.strip() == expected_headers:
+        # Skip the header line
+        return
+    
     try:
         # Split the input line into fields (assuming CSV format)
         fields = line.strip().split(',')
@@ -11,17 +15,17 @@ def map_function(line):
         # Extract the unique key (assuming it's in a specific column, adjust the index as needed)
         unique_key = fields[7]  # Change the index to the appropriate column
         
-        # Loop through predictor variables (starting from the second column)
-        predictors = fields[0:6]  # Adjust the slice to include all predictor columns
+        # Loop through predictor variables (use the specific column names)
+        pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree, age, outcome = fields[:9]
         
         # Emit key-value pairs with the unique key as the key
         # Key: unique key read from the dataset
         # Value: Indicator variable and predictor values
-        print(f'{unique_key}\t{indicator},{",".join(predictors)}')
+        print(f'{unique_key}\t{indicator},{pregnancies},{glucose},{blood_pressure},{skin_thickness},{insulin},{bmi},{diabetes_pedigree},{age},{outcome}')
 
     except Exception as e:
-        # Handle any exceptions gracefully
-        print("ERROR IN MAPPING")
+        # Handle any exceptions gracefully and print the problematic line
+        print(f"ERROR IN MAPPING: {line.strip()}")
         pass
 
 # Process input lines from standard input
